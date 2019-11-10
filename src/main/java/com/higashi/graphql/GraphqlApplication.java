f6fdd71693f530aad4f0e4498e24488904a6eb88
Fernando <fernandohig@gmail.com>
2019-11-10 14:15:48 -0300

    Graphql with spring boot

--- src/main/java/com/higashi/graphql/GraphqlApplication.java
+++ src/main/java/com/higashi/graphql/GraphqlApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GraphqlexampleApplication {
+public class GraphqlApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GraphqlexampleApplication.class, args);
+		SpringApplication.run(GraphqlApplication.class, args);
 	}
 
 }
